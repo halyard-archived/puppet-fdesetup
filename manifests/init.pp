03df4a418680aa5d08070cbe6d4b4e4198b9ce15
Les Aker <me@lesaker.org>
2014-08-31 17:16:26 -0400

    try setting user manually

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,15 +2,6 @@ class fdesetup {
   exec { 'enable-fde':
     command => 'sudo /usr/sbin/fdesetup enable -defer /tmp/fdesetup -forceatlogin 0 -norecoverykey',
     onlyif => "test '${::root_encrypted}' == 'no'",
-    require => Sudoers['fdesetup-enable']
-  }
-
-  sudoers { 'fdesetup-enable':
-    users       => $::boxen_user,
-    hosts       => 'ALL',
-    commands    => [
-      '(ALL) NOPASSWD: /usr/sbin/fdesetup enable',
-    ],
-    type        => 'user_spec',
+    user => 'root'
   }
 }
