13d20ea0ff75547c8d2796dd88d537326aea81aa
Les Aker <me@lesaker.org>
2014-09-03 18:44:14 -0400

    fix fdesetup call

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,16 @@ class fdesetup(
   $tmpfile = '/tmp/fdesetup'
 ) {
   exec { 'enable-fde':
-    command => "fdesetup enable -defer ${tmpfile} -user ${::boxen_user} -forceatlogin 0 -norecoverykey",
+    command => "fdesetup enable -defer ${tmpfile} -user ${::boxen_user}",
     path => '/usr/bin:/bin',
     onlyif => [
       "test '${::root_encrypted}' == 'no'",
       'test -z "$(fdesetup status | grep Deferred)"'
     ],
     user => 'root'
+  }
+
+  file { "${tmpfile}":
+    ensure => 'absent'
   }
 }
